9301ce935d7ff87af8b404e5182d4e381aa07443
Gsiuol31 <Louis.genest19@alumni.loyola.ca>
2018-01-19 10:20:41 -0500

    Add files via upload

--- Schedule javascript.js
+++ Schedule javascript.js
@@ -1,2 +1,2 @@
-var d = new Date("January 12, 2018 10:13:00");
-document.getElementById("lewis").innerHTML = d;
+var d = new Date("January 19, 2018");
+document.getElementById("lewis").innerHTML = Date();
